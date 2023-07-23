606359d7a6df52b24cb0faf4d2c45f24f42a6c2b
Ayoub <ayoubchebbi570@gmail.com>
2023-07-24 00:36:20 +0100

    Button Cliker

---
+++ Week 2/Day 5/Button Clicker/main.js
@@ -0,0 +1,11 @@
+function changeText(element){
+    element.innerText = "Logout";
+}
+
+function removeText(){
+    document.getElementById("btn-3").remove;
+}
+
+function likebutton(){
+    alert("Ninja Was Liked");
+}
