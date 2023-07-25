27f06610042ca9248b7c3807b5a23b19d0caab99
Ayoub <ayoubchebbi570@gmail.com>
2023-07-25 16:44:50 +0100

    profile page Assignement

---
+++ Week 3/Day 1/Practice/profile-page/script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+var userCard = document.querySelector("#remove-card");
+
+function changeonlick(){
+    userCard.remove;
+}
+
+var userName = document.querySelector("#name");
+
+function changeName(){
+    userName.innerHTML = "Ayoub Chebbi";
+}
