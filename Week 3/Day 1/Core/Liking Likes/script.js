d9c24cbb276e65c08cf4158dc354fa7e44d0554f
Ayoub <ayoubchebbi570@gmail.com>
2023-07-25 15:11:43 +0100

    liking likes Assignement

---
+++ Week 3/Day 1/Core/Liking Likes/script.js
@@ -0,0 +1,14 @@
+console.log("page loading...");
+
+// index     0  1   2
+var likes = [9, 12, 9];
+var spans = [
+    document.querySelector("#like1"),
+    document.querySelector("#like2"),
+    document.querySelector("#like3")
+];
+
+function like(id) {
+    likes[id]++;
+    spans[id].innerHTML = likes[id] + "like(s)";
+}
